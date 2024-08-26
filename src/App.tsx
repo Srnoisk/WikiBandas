5c4904f5a1c84eb23f894e37c506bd78a20e9a75
Srnoisk <106362761+Srnoisk@users.noreply.github.com>
2024-08-26 11:20:40 -0300

    asadsd

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
-
-
 import Home from './pages/home';
 import Bandas from './pages/bandas';
 import Contato from './componentes/contato/contato';
