29daa28c77a5c6c69d378f77d9c1638d59df4dd6
Srnoisk <106362761+Srnoisk@users.noreply.github.com>
2024-08-26 11:54:14 -0300

    arrumando a pagina do queen

--- src/banco/bandas/queen.tsx
+++ src/banco/bandas/queen.tsx
@@ -6,15 +6,103 @@ import '../../App.css';
 
 export default function Queen () {
     return (
-        <div className="">
-            <Header />
-            <h2>Queen</h2>
-            <div className="ftbeatles">
-            <img src="C:\Users\enrique.rlinares\Desktop\wiki2.0\src\banco\Imagens das Bandas\Queen_-_montagem.png" alt="" />
-            </div>
-            
+      <div className="">
+        <Header></Header>
+        <section>
+          <h2>Queen</h2>
+          {/* <div className="ftbeatles">
+          
+          </div> */}
+          <h3>Análise</h3>
+          <p className="informacoes_da_banda">
+            O Queen é reconhecido por sua qualidade sonora excepcional, que
+            incorpora uma ampla gama<br />
+            de estilos musicais, desde o rock clássico até o operático. Suas
+            canções são marcadas por melodias<br />
+            cativantes, arranjos elaborados e performances vocais poderosas,
+            lideradas pelo carismático Freddie<br />
+            Mercury. A habilidade técnica dos membros da banda, combinada com
+            sua criatividade e inovação,<br /> resultou em um som distintivo
+            que continua a inspirar e influenciar músicos e ouvintes até os
+            dias atuais.
+          </p>
 
-        
+          <h3>História</h3>
+          <p className="informacoes_da_banda">
+            Formado em 1970, o Queen rapidamente se destacou na cena musical
+            britânica com sua abordagem<br />
+            única e teatral ao rock. Ao longo dos anos 1970 e 1980, a banda
+            lançou uma série de álbuns icônicos,<br />
+            incluindo "A Night at the Opera" (1975) e "The Game" (1980), que
+            continham alguns de seus maiores sucessos,<br />
+            como "Bohemian Rhapsody", "We Will Rock You" e "Another One Bites
+            the Dust". O Queen também é famoso por<br />
+            suas grandiosas apresentações ao vivo, incluindo o lendário show no
+            estádio de Wembley em 1985, que é<br /> amplamente considerado um dos
+            maiores concertos da história do rock.
+          </p>
+
+          <h3>Importância Cultural</h3>
+          <p className="informacoes_da_banda">
+            O Queen é uma das bandas mais influentes e amadas da história da
+            música, deixando um legado<br />
+            duradouro que transcende gerações. Sua música continua a ser
+            amplamente celebrada e reimaginada, e sua<br />
+            abordagem ousada e inovadora ao rock ajudou a moldar o curso da
+            música popular. Além disso, Freddie Mercury,<br />
+            com sua presença de palco magnética e talento vocal extraordinário,
+            é reverenciado como um dos maiores artistas de<br />
+            todos os tempos, personificando a ideia de autenticidade e expressão
+            artística total.
+          </p>
+
+          <ul>
+            <h3>Participantes</h3>
+            <li>Freddie Mercury</li>
+            <li>Brian May</li>
+            <li>Roger Taylor</li>
+            <li>John Deacon</li>
+            <p></p>
+            <h3>Outros Integrantes</h3>
+            <li>Mike Grose</li>
+            <li>Barry Mitchel</li>
+            <li>Douglas Bogie</li>
+
+            <h3>Origem</h3>
+            <li>
+              <p>Londres, Inglaterra</p>
+            </li>
+
+            <h3>Discografia</h3>
+            <ul>
+              <li>Queen - 13 de Julho de 1973</li>
+              <li>Queen II - 8 de março de 1974</li>
+              <li>Sheer Heart Attack - 8 de Novembro de 1974</li>
+              <li>A Night at the Opera - 21 de Novembro de 1975</li>
+              <li>A Day at the Races - 10 de Dezembro de 1976</li>
+              <li>News of the World - 28 de Outubro de 1977</li>
+              <li>Jazz - 10 de Novembro de 1978</li>
+              <li>The Game - 30 de Junho de 1980</li>
+              <li>Flash Gordon - 8 de Dezembro de 1980</li>
+              <li>Hot Space - 21 de Maio de 1982</li>
+              <li>The Works - 27 de Fevereiro de 1984</li>
+              <li>A Kind of Magic - 2 de Junho de 1986</li>
+              <li>The Miracle - 22 de Maio de 1989</li>
+              <li>Innuendo - 4 de Fevereiro 1991</li>
+              <li>Made in Heaven - 6 de Novembro de 1995</li>
+              <li>The Cosmos Rocks - 15 de Setembro de 2008</li>
+            </ul>
+            <h3>Links Externos</h3>
+            <ul>
+              <li>
+                <a href="https://queenonline.com/">Website Oficial</a>
+              </li>
+              <li>
+                <a href="http://facebook.com/exemplo">Página no Facebook</a>
+              </li>
+            </ul>
+          </ul>
+        </section>
         </div>
     )
 }
